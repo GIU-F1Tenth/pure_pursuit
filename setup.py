25e60530c170a75eeced58fb5751d2f3791077ac
george10hany77 <georgehany064@gmail.com>
2025-05-26 13:41:25 +0000

    moving the path dir to trajectory_planning pkg

--- setup.py
+++ setup.py
@@ -25,8 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pure_pursuit_node = pure_pursuit.pure_pursuit_node:main',
-            "csv_pub_exe = pure_pursuit.csv_path_publisher:main"
+            'pure_pursuit_node = pure_pursuit.pure_pursuit_node:main'
         ],
     },
 )
