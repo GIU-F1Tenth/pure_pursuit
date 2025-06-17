137e4bf1e975ddb5a014a1bca8220440add12b90
= <=>
2025-06-17 13:26:13 +0200

    addign the solo config and solo launch

---
+++ launch/pure_pursuit_solo.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python import get_package_share_directory
+import os
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    
+    param_path = os.path.join(get_package_share_directory("pure_pursuit"), "config", "params_solo.yaml")
+    pure_pursuit = Node(
+            package='pure_pursuit',
+            executable='pure_pursuit_node',
+            name='pure_pursuit_node',
+            parameters=[param_path],
+            output='screen'
+        )
+    
+    ld.add_action(pure_pursuit)
+
+    return ld
