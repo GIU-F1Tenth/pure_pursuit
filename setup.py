62e480eda579c6ac94829599ca25f4d5e133ab18
Fam Awad Louiz Awad Shihata <fam@awadlouis.com>
2025-09-07 11:22:55 +0200

    docs: updated readme for new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'pure_pursuit'
 
 setup(
     name=package_name,
-    version='1.1.1',
+    version='1.1.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
