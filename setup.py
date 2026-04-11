5527d0f18934efbf90ea37e491764d0e43c8403e
FamALouiz <fam@awadlouis.com>
2026-04-11 15:05:17 +0200

    feat: add csv path pub

--- setup.py
+++ setup.py
@@ -2,34 +2,31 @@ import os
 from glob import glob
 from setuptools import setup
 
-package_name = 'pure_pursuit'
+package_name = "pure_pursuit"
 
 setup(
     name=package_name,
-    version='1.1.2',
+    version="1.1.2",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         # Include all launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
         # Include config files
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        # Include path files
-        (os.path.join('share', package_name, 'path'), glob('path/*.csv')),
-        # Include license
-        (os.path.join('share', package_name), ['LICENSE']),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name), ["LICENSE"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Fam Shihata, George Halim, Karim Shousha',
-    maintainer_email='fam@awadlouis.com, georgehany064@gmail.com',
-    description='Pure Pursuit path following controller for F1TENTH autonomous racing',
-    license='MIT',
+    maintainer="Fam Shihata, George Halim, Karim Shousha",
+    maintainer_email="fam@awadlouis.com, georgehany064@gmail.com",
+    description="Pure Pursuit path following controller for F1TENTH autonomous racing",
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'pure_pursuit_node = pure_pursuit.pure_pursuit_node:main'
+        "console_scripts": [
+            "pure_pursuit_node = pure_pursuit.pure_pursuit_node:main"
+            "csv_path_pub = pure_pursuit.csv_path_publisher:main"
         ],
     },
 )
